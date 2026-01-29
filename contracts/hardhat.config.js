e8404fa323a6e8481cc147e8496d3612972b890a
0xward <aradeawardana97@gmail.com>
2026-01-29 08:18:39 +0700

    Add Hardhat configuration for Solidity and network

---
+++ contracts/hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    base: {
+      url: "https://mainnet.base.org",
+      accounts: ["<c6dhebjj4440zzMnbFk>"]
+    }
+  }
+};
